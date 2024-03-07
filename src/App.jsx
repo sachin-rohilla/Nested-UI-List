15fbfadc4e526d269f7d93b56e7e29d7b2ef22ea
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-07 12:15:40 +0530

    fix-code

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import { NestedData } from "./constants/constants";
 function App() {
   const [showCategoryList, setShowCategoryList] = useState(false);
   const [showSubCategoryList, setShowSubCategoryList] = useState(false);
+  const [showSubCategoryImage, setShowSubCategoryImage] = useState(false);
   const [depmartMentId, setDepartmentId] = useState(null);
   const [categoryId, setCategoryId] = useState(null);
+  const [subCategoryId, setSubCategoryId] = useState(null);
 
   return (
     <>
@@ -17,11 +19,12 @@ function App() {
           {NestedData?.length > 0 &&
             NestedData?.map((deptItem) => (
               <div key={deptItem?.deptId}>
-                <div className="w-44 h-42 overflow-y-auto border rounded-sm  ">
+                <div className="w-44 h-42 overflow-y-auto border rounded-sm  shadow-md ">
                   <h1
                     onClick={() => {
                       setShowCategoryList(true);
                       setDepartmentId(deptItem?.deptId);
+                      setShowSubCategoryList(false);
                     }}
                     className={`hover:bg-yellow-400 hover:text-white px-4 py-2  cursor-pointer ${
                       depmartMentId === deptItem?.deptId
@@ -42,11 +45,12 @@ function App() {
                       )
                       ?.map((catItem) => (
                         <div key={catItem?.catId}>
-                          <div className="w-44 h-42 overflow-y-auto border rounded-sm   ">
+                          <div className="w-44 h-42 overflow-y-auto border rounded-sm shadow-md   ">
                             <h1
                               onClick={() => {
                                 setShowSubCategoryList(true);
                                 setCategoryId(catItem?.catId);
+                                setShowSubCategoryImage(false);
                               }}
                               className={`hover:bg-yellow-400 hover:text-white px-4 py-2  cursor-pointer
                               ${
@@ -71,11 +75,41 @@ function App() {
                                 )
                                 ?.map((subCatItem) => (
                                   <div key={subCatItem?.subId}>
-                                    <div className="w-44 h-42 overflow-y-auto border rounded-sm   ">
-                                      <h1 className=" hover:bg-yellow-400 hover:text-white px-4 py-2  cursor-pointer">
+                                    <div className="w-44 h-42 overflow-y-auto border rounded-sm shadow-md   ">
+                                      <h1
+                                        className={`hover:bg-yellow-400 hover:text-white px-4 py-2  cursor-pointer 
+                                            ${
+                                              subCategoryId ===
+                                              subCatItem?.subId
+                                                ? "bg-yellow-400 text-white"
+                                                : ""
+                                            }
+                                            `}
+                                        onClick={() => {
+                                          setShowSubCategoryImage(true);
+                                          setSubCategoryId(subCatItem?.subId);
+                                        }}
+                                      >
                                         {subCatItem?.subCategoryName}
                                       </h1>
                                     </div>
+                                    {/* subcategorylist start */}
+                                    {showSubCategoryImage &&
+                                      subCatItem?.subId === subCategoryId && (
+                                        <div className=" absolute top-0 left-48 w-52 h-52 border rounded shadow-md">
+                                          {subCatItem?.subCategoryImage ? (
+                                            <img
+                                              src={subCatItem?.subCategoryImage}
+                                              alt="subCategoryImage"
+                                            />
+                                          ) : (
+                                            <>
+                                              <img src="https://media.istockphoto.com/id/1392182937/vector/no-image-available-photo-coming-soon.jpg?s=612x612&w=0&k=20&c=3vGh4yj0O2b4tPtjpK-q-Qg0wGHsjseL2HT-pIyJiuc=" />
+                                            </>
+                                          )}
+                                        </div>
+                                      )}
+                                    {/* subcategorylist end */}
                                   </div>
                                 ))}
                           </div>
--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -53,6 +53,8 @@ export const NestedData = [
             catId: 1,
             subId: 1,
             subCategoryName: "Apple",
+            subCategoryImage:
+              "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1693086369818",
           },
           {
             catId: 1,
